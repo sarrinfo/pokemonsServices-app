e5a2a5a12f14454ad338840da44ef626dabcc732
sarrinfo <ibrahima.sarr161@gmail.com>
2019-05-29 22:00:01 -0400

    vo

---
+++ ApiPokemon/App_Start/WebApiConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace ApiPokemon
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Configuration et services API Web
+            config.EnableCors();
+
+            // Itinéraires de l'API Web
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            var appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml");
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
+        }
+    }
+}
---
+++ ApiPokemon/Controllers/pokemonsController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.Http.Description;
+using System.Web.Mvc;
+using ApiPokemon;
+
+namespace ApiPokemon.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class pokemonsController : ApiController
+    {
+        private SARRINFODBPokemonEntities db = new SARRINFODBPokemonEntities();
+
+        // GET: api/pokemons
+        public IQueryable<pokemon> Getpokemon()
+        {
+            return db.pokemon;
+        }
+
+        // GET: api/pokemons/5
+        [ResponseType(typeof(pokemon))]
+        public async Task<IHttpActionResult> Getpokemon(int id)
+        {
+            pokemon pokemon = await db.pokemon.FindAsync(id);
+            if (pokemon == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(pokemon);
+        }
+
+        // PUT: api/pokemons/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> Putpokemon(int id, pokemon pokemon)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != pokemon.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(pokemon).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!pokemonExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/pokemons
+        [ResponseType(typeof(pokemon))]
+        public async Task<IHttpActionResult> Postpokemon(pokemon pokemon)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.pokemon.Add(pokemon);
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (pokemonExists(pokemon.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = pokemon.Id }, pokemon);
+        }
+
+        // DELETE: api/pokemons/5
+        [ResponseType(typeof(pokemon))]
+        public async Task<IHttpActionResult> Deletepokemon(int id)
+        {
+            pokemon pokemon = await db.pokemon.FindAsync(id);
+            if (pokemon == null)
+            {
+                return NotFound();
+            }
+
+            db.pokemon.Remove(pokemon);
+            await db.SaveChangesAsync();
+
+            return Ok(pokemon);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool pokemonExists(int id)
+        {
+            return db.pokemon.Count(e => e.Id == id) > 0;
+        }
+    }
+}
